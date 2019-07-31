439e9e93068fc4d601871e0bddef2390e7f3d9df
Vu Le <vulevu121@gmail.com>
2019-07-30 21:45:34 -0700

    Added watch folder tab and buttons

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,3 +1,5 @@
+#include <QStandardItemModel>
+#include <QStandardItem>
 #include "MainWindow.h"
 #include "ui_MainWindow.h"
 
@@ -6,9 +8,65 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    settingsDialog = new SettingsDialog();
+    connect(ui->actionSetting, SIGNAL(triggered()), this->settingsDialog, SLOT(show()));
+
+    QStringList queueHeader({
+        "Filename",
+        "Album",
+        "Status",
+        "Date Added",
+        "Path",
+    });
+    QStandardItemModel *queueModel = new QStandardItemModel();
+    queueModel->setHorizontalHeaderLabels(queueHeader);
+
+    ui->queueTableView->setModel(queueModel);
+
+    QList<QStandardItem *> queueRow({
+        new QStandardItem("photo.jpg"),
+        new QStandardItem("MyAlbum"),
+        new QStandardItem("Queue"),
+        new QStandardItem("7/29/2019 8:32PM"),
+        new QStandardItem("/home/pictures/photo.jpg")
+    });
+
+    queueModel->appendRow(queueRow);
+
+    ui->queueTableView->resizeColumnsToContents();
+
+    QStringList watchHeader({
+        "Folder",
+        "Status",
+        "No. Files",
+        "Last Scanned",
+        "Last Modified",
+        "Path",
+    });
+    QStandardItemModel *watchModel = new QStandardItemModel();
+    watchModel->setHorizontalHeaderLabels(watchHeader);
+
+    ui->watchTableView->setModel(watchModel);
+
+    QList<QStandardItem *> watchRow({
+        new QStandardItem("MyPictures"),
+        new QStandardItem("Scanned"),
+        new QStandardItem("1"),
+        new QStandardItem("7/29/2019 8:32PM"),
+        new QStandardItem("7/29/2019 8:32PM"),
+        new QStandardItem("/home/My Pictures")
+    });
+
+    watchModel->appendRow(watchRow);
+
+    ui->watchTableView->resizeColumnsToContents();
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
--- MainWindow.h
+++ MainWindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "SettingsDialog.h"
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +16,12 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+
+
 private:
     Ui::MainWindow *ui;
+    SettingsDialog *settingsDialog = nullptr;
 };
 
 #endif // MAINWINDOW_H
